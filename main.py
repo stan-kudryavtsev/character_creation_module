9057996522d1e09209469cb0736b1c0bfc582b1e
Stan Kudryavtsev <stan.kudryavtsev@yandex.ru>
2023-04-22 13:59:03 +0300

    add docstrings

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Выбираем значение атаки по классу
+    если персонаж безклассовый, то атаковать ему нечем"""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -17,6 +19,8 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Выбираем значение защиты по классу
+    если персонаж без класса, то блокировать удары ему нечем"""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +31,8 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Выбираем ульту по классу, если персонаж без класса
+    то ульты у него нет"""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -40,6 +46,8 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция тренировки, чтобы попробовать атаку, блок и ульту
+    в тренировочном режиме"""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -64,6 +72,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора класса персонажа с подтверждением"""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
